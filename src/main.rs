42c147164bcdd6db75e22c68cce5c7793c5c00c6
b13v <yba@proton.me>
2023-11-07 16:42:49 +0600

    Adding struct and two functions

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+#[derive(Debug)]
+
+struct Arguments {
+    target: String,
+    replacement: String,
+    filename: String,
+    output: String,
 }
+
+use text_colorizer::*;
+
+fn print_usage() {
+    eprintln!(
+        "{} - change occurences of one string into another",
+        "quickreplace".green()
+    );
+    eprintln!("Usage: quickreplace <target> <replacement> <INPUT> <OUTPUT>");
+}
+
+fn main() {}
