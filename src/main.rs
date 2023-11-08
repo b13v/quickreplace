65b53a3249a43d3ac14304f9a4461dcfccc3ec94
b13v <yba@proton.me>
2023-11-08 12:05:06 +0600

    Adding `reading and writing files` code

--- src/main.rs
+++ src/main.rs
@@ -17,7 +17,7 @@ fn print_usage() {
     eprintln!("Usage: quickreplace <target> <replacement> <INPUT> <OUTPUT>");
 }
 
-use std::env;
+use std::{env, fs};
 
 fn parse_args() -> Arguments {
     let args: Vec<String> = env::args().skip(1).collect();
@@ -41,6 +41,31 @@ fn parse_args() -> Arguments {
 }
 
 fn main() {
-    let agrs = parse_args();
-    println!("{:?}", agrs);
+    let args = parse_args();
+
+    let data = match fs::read_to_string(&args.filename) {
+        Ok(v) => v,
+        Err(e) => {
+            eprintln!(
+                "{} failed to read from file '{}': {:?}",
+                "Error:".red().bold(),
+                args.filename,
+                e
+            );
+            std::process::exit(1);
+        }
+    };
+
+    match fs::write(&args.output, &data) {
+        Ok(_) => {}
+        Err(e) => {
+            eprintln!(
+                "{} failed to write to file '{}': {:?}",
+                "Error:".red().bold(),
+                args.filename,
+                e
+            );
+            std::process::exit(1);
+        }
+    };
 }
