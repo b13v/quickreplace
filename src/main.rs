2eedbb324db2ee2f8c068a58dd969aa6e52cac3c
b13v <yba@proton.me>
2023-11-08 11:07:16 +0600

    Adding a `parse_args` function and printing commmand line arguments

--- src/main.rs
+++ src/main.rs
@@ -17,4 +17,30 @@ fn print_usage() {
     eprintln!("Usage: quickreplace <target> <replacement> <INPUT> <OUTPUT>");
 }
 
-fn main() {}
+use std::env;
+
+fn parse_args() -> Arguments {
+    let args: Vec<String> = env::args().skip(1).collect();
+
+    if args.len() != 4 {
+        print_usage();
+        eprintln!(
+            "{} wrong number of arguments: expected 4, got {}.",
+            "Error:".red().bold(),
+            args.len()
+        );
+        std::process::exit(1);
+    }
+
+    Arguments {
+        target: args[0].clone(),
+        replacement: args[1].clone(),
+        filename: args[2].clone(),
+        output: args[3].clone(),
+    }
+}
+
+fn main() {
+    let agrs = parse_args();
+    println!("{:?}", agrs);
+}
