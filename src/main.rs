94bb563f2d7b44c9d3466027dce367e9ff8fc459
b13v <yba@proton.me>
2023-11-09 12:12:00 +0600

    Adding regex crate and final touch on main function

--- src/main.rs
+++ src/main.rs
@@ -40,6 +40,12 @@ fn parse_args() -> Arguments {
     }
 }
 
+use regex::Regex;
+fn replace(target: &str, replacement: &str, text: &str) -> Result<String, regex::Error> {
+    let regex = Regex::new(target)?;
+    Ok(regex.replace_all(text, replacement).to_string())
+}
+
 fn main() {
     let args = parse_args();
 
@@ -56,7 +62,15 @@ fn main() {
         }
     };
 
-    match fs::write(&args.output, &data) {
+    let replaced_data = match replace(&args.target, &args.replacement, &data) {
+        Ok(v) => v,
+        Err(e) => {
+            eprintln!("{} failed to replace text: {:?}", "Error:".red().bold(), e);
+            std::process::exit(1);
+        }
+    };
+
+    match fs::write(&args.output, &replaced_data) {
         Ok(_) => {}
         Err(e) => {
             eprintln!(
